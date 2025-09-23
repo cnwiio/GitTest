07d9fd0be1e7f936159d755d12226594e56c5ba5
CAMT <kritgoodgame@gmail.com>
2025-09-23 15:07:53 +0700

    update playerJump

--- game/Game1.cs
+++ game/Game1.cs
@@ -6,7 +6,7 @@ namespace game
 {
     public class Game1 : Game
     {
-        // “update feature3
+        // update playerJump
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
