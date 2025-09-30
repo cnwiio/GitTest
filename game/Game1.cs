fd4dfe001111a6a37ecf86fd45c215dc463dc5cb
CAMT <thachapol05@gmail.com>
2025-09-30 14:50:28 +0700

    update feature1

--- game/Game1.cs
+++ game/Game1.cs
@@ -10,6 +10,8 @@ namespace game
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private int UzawaReisa = 69;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
