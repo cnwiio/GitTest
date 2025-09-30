f6f5b073a4d2be6c37e69e3349c4e49e4d99257e
CAMT <kritgoodgame@gmail.com>
2025-09-30 14:49:54 +0700

    update feature1

--- game/Game1.cs
+++ game/Game1.cs
@@ -37,7 +37,7 @@ namespace game
                 Exit();
 
             // TODO: Add your update logic here
-
+            // Edited in Feature_Walking
             base.Update(gameTime);
         }
 
